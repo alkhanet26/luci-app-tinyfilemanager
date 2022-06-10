33999f37c717288cddebbdf4d18cecd1c8ed8977
lynxnexy <81577496+lynxnexy@users.noreply.github.com>
2022-06-10 14:57:25 +0700

    Update tinyfilemanager.sh

--- root/etc/uci-defaults/tinyfilemanager.sh
+++ root/etc/uci-defaults/tinyfilemanager.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
-uci set uhttpd.main.interpreter='.php=/usr/bin/php-cgi'
-uci add_list uhttpd.main.index_page='index.php'
-uci commit uhttpd
-/etc/init.d/uhttpd restart
-rm -f /tmp/luci-indexcache
-rm -rf /tmp/luci-modulecache
 
 cd /
 ln -s / /www/tinyfilemanager/rootfs
